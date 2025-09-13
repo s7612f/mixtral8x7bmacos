1709adb2eda4a9447dfe41fe8653392ee84043aa
s7612f <75951407+s7612f@users.noreply.github.com>
2025-09-13 23:43:07 +0100

    Improve DMG packaging and installer

--- build_dmg.sh
+++ build_dmg.sh
@@ -8,8 +8,13 @@ DMG_NAME="mixtral8x7b.dmg"
 rm -rf build dist
 mkdir -p "build/$APP_NAME" dist
 
-cp install.sh "build/$APP_NAME/"
+# Copy installer and assets
+cp install.sh "build/$APP_NAME/install.command"
+cp README.txt "build/$APP_NAME/README.txt"
 cp -r webgui "build/$APP_NAME/webgui"
+
+# Ensure installer has execute permissions
+chmod 755 "build/$APP_NAME/install.command"
 
 hdiutil create "dist/$DMG_NAME" -volname "$VOL_NAME" -srcfolder build -ov -format UDZO
 
